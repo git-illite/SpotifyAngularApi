96f573351c3bd46149f0ccb32d5e58a96d351d26
git-illite <abdallah.aden@gmail.com>
2023-08-21 13:52:37 -0400

    Update user-service.js

--- UserApi/user-service.js
+++ UserApi/user-service.js
@@ -19,7 +19,7 @@ let User;
 
 module.exports.connect = function () {
     return new Promise(function (resolve, reject) {
-        let db = mongoose.createConnection(mongoDBConnectionString, { useNewUrlParser: true });
+        let db = mongoose.createConnection(mongoDBConnectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 
         db.on('error', err => {
             reject(err);
